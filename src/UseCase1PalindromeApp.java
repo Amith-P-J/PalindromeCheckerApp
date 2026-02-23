8d5ad97301617f78bf63f9483ec3d2c1e5d34506
Amith-P-J <amithpj05@gmail.com>
2026-02-23 10:19:14 +0530

    UC3String Reverse:Hardcoded palindrome

--- src/UseCase1PalindromeApp.java
+++ src/UseCase1PalindromeApp.java
@@ -1,30 +1,44 @@
 public class UseCase1PalindromeApp {
+
+
     public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
-        String input= "madam";
-        System.out.println("Input Text: " + input);
 
+        //USE CASE 1
+//        System.out.println("Welcome to the Palindrome Checker Management System");
+//        System.out.println("Version : 1.0");
 
-        if(input== null || input.trim().isEmpty()){
-            System.out.println("Empty String, Please enter a String");
-            return;
+        //USE CASE 2
+//        String input= "madam";
+//        if(input== null || input.trim().isEmpty()){
+//            System.out.println("Empty String, Please enter a String");
+//            return;//        }
+//        boolean plaindrome = true;
+//        for(int i=0;i<input.length()/2;i++){
+//            if(input.charAt(i) != input.charAt((input.length())-i-1)) {
+ //               plaindrome = false;
+ //           }
+ //       }
+ //       if(plaindrome){
+  //          System.out.println("Is it a palindrome : "+plaindrome);
+  //      }
+ //       else{
+//            System.out.println("Is it a palindrome : "+plaindrome);
+//        }
+
+        String input ="madam";
+        System.out.println("Input Text: "+ input);
+
+        //USE CASE 3
+
+        String rev = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            rev = rev + input.charAt(i);
         }
-
-
-        boolean plaindrome = true;
-        for(int i=0;i<input.length()/2;i++){
-            if(input.charAt(i) != input.charAt((input.length())-i-1)) {
-                plaindrome = false;
-            }
-        }
-        if(plaindrome){
-            System.out.println("Is it a palindrome : "+plaindrome);
-        }
-        else{
-            System.out.println("Is it a palindrome : "+plaindrome);
-
+        if (input.equals(rev)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
         }
     }
 }

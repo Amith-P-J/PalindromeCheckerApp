2d1ee8e5d90ddc8329b763e2265acea688ad8795
Amith-P-J <amithpj05@gmail.com>
2026-02-23 09:37:51 +0530

    UC2:Hardcoded palindrome

--- src/UseCase1PalindromeApp.java
+++ src/UseCase1PalindromeApp.java
@@ -3,5 +3,28 @@ public class UseCase1PalindromeApp {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully.");
+        String input= "madam";
+        System.out.println("Input Text: " + input);
+
+
+        if(input== null || input.trim().isEmpty()){
+            System.out.println("Empty String, Please enter a String");
+            return;
+        }
+
+
+        boolean plaindrome = true;
+        for(int i=0;i<input.length()/2;i++){
+            if(input.charAt(i) != input.charAt((input.length())-i-1)) {
+                plaindrome = false;
+            }
+        }
+        if(plaindrome){
+            System.out.println("Is it a palindrome : "+plaindrome);
+        }
+        else{
+            System.out.println("Is it a palindrome : "+plaindrome);
+
+        }
     }
 }

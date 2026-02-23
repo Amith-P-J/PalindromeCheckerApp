c7d6dd5bd59e49f5389673b8d97164b08cde1006
Amith-P-J <amithpj05@gmail.com>
2026-02-23 11:01:42 +0530

    UC5:Stack palindrome

--- src/UseCase1PalindromeApp.java
+++ src/UseCase1PalindromeApp.java
@@ -1,35 +1,37 @@
+import java.util.*;
 public class UseCase1PalindromeApp {
 
 
     public static void main(String[] args){
 
         //USE CASE 1
-//        System.out.println("Welcome to the Palindrome Checker Management System");
-//        System.out.println("Version : 1.0");
+        /*
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
 
         //USE CASE 2
-//        String input= "madam";
-//        if(input== null || input.trim().isEmpty()){
-//            System.out.println("Empty String, Please enter a String");
-//            return;//        }
-//        boolean plaindrome = true;
-//        for(int i=0;i<input.length()/2;i++){
-//            if(input.charAt(i) != input.charAt((input.length())-i-1)) {
- //               plaindrome = false;
- //           }
- //       }
- //       if(plaindrome){
-  //          System.out.println("Is it a palindrome : "+plaindrome);
-  //      }
- //       else{
-//            System.out.println("Is it a palindrome : "+plaindrome);
-//        }
-
-        String input ="madam";
+        String input= "madam";
+        if(input== null || input.trim().isEmpty()){
+            System.out.println("Empty String, Please enter a String");
+            return;//        }
+        boolean plaindrome = true;
+        for(int i=0;i<input.length()/2;i++){
+            if(input.charAt(i) != input.charAt((input.length())-i-1)) {
+                plaindrome = false;
+            }
+       }
+       if(plaindrome){
+          System.out.println("Is it a palindrome : "+plaindrome);
+       }
+       else{
+            System.out.println("Is it a palindrome : "+plaindrome);
+        }
+*/
+        String input ="noon";
         System.out.println("Input Text: "+ input);
 
         //USE CASE 3
-
+/*
         String rev = "";
 
         for (int i = input.length() - 1; i >= 0; i--) {
@@ -40,5 +42,44 @@ public class UseCase1PalindromeApp {
         } else {
             System.out.println(input + " is not a palindrome.");
         }
+        */
+/*
+        //USE CASE 4
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean Palindrome = true;
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                Palindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        System.out.println("Is it a palindrome: " + Palindrome);
+
+*/
+        //USE CASE 5
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        boolean isPalindrome = true;
+        for (char c : input.toCharArray()) {
+            char reversedChar = stack.pop();
+
+            if (c != reversedChar) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+
     }
 }
